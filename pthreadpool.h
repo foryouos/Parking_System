29cd3a9be951970680f7be1f8ddd971d4526658c
瓶子的跋涉 <58133210+foryouos@users.noreply.github.com>
2025-10-23 22:31:38 +0800

    添加所需头文件

--- pthreadpool.h
+++ pthreadpool.h
@@ -10,6 +10,7 @@ class PthreadPool_Plate : public QObject,public QRunnable
     Q_OBJECT
 public:
     explicit  PthreadPool_Plate(QObject *parent = nullptr);
+    void RecvPlate(mysql* mysql,QString license_plate, QString formattedDateTime, QString location);
     // 在子类中必须要重写的函数，里面的任务是处理流程
     void run() override;
 public slots:
